9416a6fa77e2f53f118470332203f2e3cfef7193
luis86129 <luismendoza86129@gmail.com>
2026-09-02 11:15:11 -0500

    Primer commit en linux

--- PracticasJavaFX/src/main/java/org/example/Main.java
+++ PracticasJavaFX/src/main/java/org/example/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("Sistema Farmacia");
+        primaryStage.setTitle("Sistema Farmacia Hola");
         DatabaseManager.initDatabase(); // <-- Inicializa SQLite
         // Cargar el icono desde los recursos
         Image icon = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/icon.png")));
